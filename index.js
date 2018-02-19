953e1b221aaebddf9f3bad24bca884e852a3336e
Dennis Hübner <dennis.huebner89@gmail.com>
2018-02-20 00:38:39 +0100

    iidk

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const URL = process.env.URL || '';
 const BOT_TOKEN = process.env.BOT_TOKEN || ''
 
 const bot = new Telegraf(BOT_TOKEN);
-bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`)
-bot.startWebhook(`bot${BOT_TOKEN}`, null, PORT)
+bot.telegram.setWebhook(`${URL}bot${BOT_TOKEN}`)
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
 
 async function fetchMealPlan () {
   // if (query === "") return {};
